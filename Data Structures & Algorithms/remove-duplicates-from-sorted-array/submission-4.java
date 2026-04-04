7dd5a9d49f88671bfe37dde15524eb630a9e00a0
Abinash Barman <129306070+abhi-bar@users.noreply.github.com>
2026-04-05 00:28:49 +0530

    Add: remove-duplicates-from-sorted-array - submission-4

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int l =0;
+        int r = 1;
+
+        int count = 0;
+
+        while (l<nums.length-1){
+            if((l+count)==nums.length-1)break;
+            // if(r==nums.length){
+            //     l+=1;
+            //     r=l+1;
+            // }
+            if(r<nums.length && nums[r]==nums[l]){
+                count++;
+                leftShift(nums, r);
+            }else{
+                l+=1;
+                r=l+1;
+            }
+        }
+        return nums.length-count;
+    }
+
+    private void leftShift(int[] nums, int i){
+        while (i<nums.length-1){
+            nums[i]=nums[i+1];
+            i++;
+        }
+    }
+}
