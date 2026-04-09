5246f02fcc32cf70d1ef576f4da8f1402c907e82
Abinash Barman <129306070+abhi-bar@users.noreply.github.com>
2026-04-10 03:47:02 +0530

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+        List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        helper(nums,0);
+        return ans;
+    }
+
+    private void helper(int[] nums, int index){
+        if(index==nums.length-1){
+            List<Integer> list1 = new ArrayList<>();
+            for(int i:nums){
+                list1.add(i);
+            }
+            ans.add(new ArrayList<>(list1));
+        }
+
+        for(int i=index; i<nums.length; i++){
+            swap(nums,i,index);
+            helper(nums,index+1);
+            swap(nums,i,index);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
